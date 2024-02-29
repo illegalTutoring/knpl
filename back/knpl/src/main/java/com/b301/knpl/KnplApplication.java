14752d8714f90abded8c70d64082a3c5594d2b1b
박예원 <parkyw0321@gmail.com>
2024-02-29 16:35:18 +0900

    init(project): 백 프로젝트 초기환경 세팅

---
+++ back/knpl/src/main/java/com/b301/knpl/KnplApplication.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:103b051d32568ab30c46fc027928f4d55255b381b12bc3d8df881bfbce9da28c
+size 314
---
+++ back/knpl/src/test/java/com/b301/knpl/KnplApplicationTests.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b905a5e10c841bc2b7c5b6798d41f748f805b66a5dfa40764096d2a428faeb1c
+size 212
