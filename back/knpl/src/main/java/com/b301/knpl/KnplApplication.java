4d4714dfc5d0e8df6967ce106313a2aa0fa1b190
박예원 <parkyw0321@gmail.com>
2024-02-29 16:35:18 +0900

    init(project): 백 프로젝트 초기환경 세팅
    Former-commit-id: ea7cd3c613d7c8013a8f3492ef51b4c429b2c0b8

---
+++ back/knpl/src/main/java/com/b301/knpl/KnplApplication.java
@@ -0,0 +1,13 @@
+package com.b301.knpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnplApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnplApplication.class, args);
+    }
+
+}
---
+++ back/knpl/src/test/java/com/b301/knpl/KnplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b301.knpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnplApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
