c0b144fe177f5f1f99537884359019042d438ef4
정종욱 <ddidue>
2024-03-04 14:24:25 +0900

    feat. 네브바 와이어 프레임 구조 작업 완료.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:777c20c5f2daea237898e050eda83f772b693a72b51bd2ed8de05eeab54ef1df
-size 242
+oid sha256:14db93eb7d5eb98cb15e73595d0af03ca78d0b548ec96521894955bb5814689c
+size 214
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22e0f121aa5da2c13f232f06bc6bab1d8f388506312d60bb854c82f5a4201cc3
-size 151
+oid sha256:aeebe1503cf2e44c730ba503e1f3fbe116db5cf909aaffdd8ac092cec52becc5
+size 159
