b69cf25588f53ae5c86f86f71d2854a65af21535
정종욱 <ddidue>
2024-03-04 17:19:32 +0900

    feat 이미지캐로젤 완성.
    Former-commit-id: 5a9328e0433c9735e29e979293b249ef44776040

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d5c235bc0d530db31a321ec3e566521462d205ec75adad65b4d46fae086992e7
-size 1065
+oid sha256:ff8e3e9db822abc7edd013350cc1b0d5ff6e3bb26c6e265e40a9f93c632818d3
+size 2239
