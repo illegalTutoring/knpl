9ea6cfd86221a9f3cca100ac99042bbdaeac72de
정종욱 <ddidue>
2024-03-05 11:25:42 +0900

    fix: navbar 컴포넌트 App.vue에서 분리
    Former-commit-id: a7003bd3b7172981b7186b1c366a066ca53cc375

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff8e3e9db822abc7edd013350cc1b0d5ff6e3bb26c6e265e40a9f93c632818d3
-size 2239
+oid sha256:3ad58a75d87e7fa445b28ff7459ee8398327996e09b28f4fc381639486ecf499
+size 1603
---
+++ frontend/src/components/basicscreennavbar/navbar.vue
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:28a2a128aef9c43a53e72341dd2254eba3eb64ddac7239fb2889e765ecb545c4
+size 1189
