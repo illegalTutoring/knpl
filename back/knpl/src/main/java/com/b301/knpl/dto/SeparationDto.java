c2e0c4f1ca50587c71278149c95da5fc22b42b03
박예원 <parkyw0321@gmail.com>
2024-03-05 14:26:30 +0900

    feat(entity): entity 생성
    Former-commit-id: 90a0db1e3ed9c5bf413efe1b811f0eea52d71ec9

--- back/knpl/src/main/java/com/b301/knpl/dto/SeparationDto.java
+++ back/knpl/src/main/java/com/b301/knpl/dto/SeparationDto.java
@@ -1,4 +1,9 @@
 package com.b301.knpl.dto;
 
 public class SeparationDto {
+
+    private String vocal;
+    private String drum;
+    private String  bass;
+    private String session;
 }
---
+++ back/knpl/src/main/java/com/b301/knpl/entity/Task.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f71f56ed2f701f12f3f6746a72ad05d28ab0274329ff04aa8c90d138c1e5ef53
+size 896
