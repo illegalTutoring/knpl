4b5bdca518e30d4817acacbd627470ada6fe1e82
정종욱 <cnhug3@naver.com>
2024-03-05 13:34:54 +0900

    fix(navbar): 네브바 요소 추가
    (주의)아직 해당 페이지로의 라우터 연결 안함.
    
    
    Former-commit-id: c15397ecf5ab13874f7bec6d30cc7ad1c2a3e5c5

--- frontend/src/components/basicscreennavbar/navbar.vue
+++ frontend/src/components/basicscreennavbar/navbar.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:28a2a128aef9c43a53e72341dd2254eba3eb64ddac7239fb2889e765ecb545c4
-size 1189
+oid sha256:26667dde70ca944bcc0d7fbf760c8758fb4402b9ca068216739a4d192a0410e1
+size 1377
