83818ec83d4118be1fc3d16d2ac6fbbb0daa78a3
정종욱 <ddidue>
2024-03-05 11:17:50 +0900

    fix: autoChangeTimeout 타입 지정
    Former-commit-id: 7c93a2f32564d61c8f1c1a74919baae316721223

--- frontend/src/components/homeviewcomponents/ImageSlideComponent.vue
+++ frontend/src/components/homeviewcomponents/ImageSlideComponent.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a5e7b785f083d931b46fcb64e334bb421d061c8835d76553250affa7e425dec
-size 1288
+oid sha256:f9f0542c5a400eb26f5ca00b868e7ed92e0205a24a28d64ee77bc5d31c57f4c0
+size 1307
