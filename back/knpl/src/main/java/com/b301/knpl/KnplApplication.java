f193d3cecc2887f042870153f3c5cb2b908567a3
박예원 <parkyw0321@gmail.com>
2024-03-05 16:43:47 +0900

    feat(entity) mongodb 관련 설정 추가
    Former-commit-id: 641d134d98794a3d8f149da9ec7ee5a793f2ff24

--- back/knpl/src/main/java/com/b301/knpl/KnplApplication.java
+++ back/knpl/src/main/java/com/b301/knpl/KnplApplication.java
@@ -2,8 +2,10 @@ package com.b301.knpl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class KnplApplication {
 
     public static void main(String[] args) {
--- back/knpl/src/main/java/com/b301/knpl/config/MongoConfig.java
+++ back/knpl/src/main/java/com/b301/knpl/config/MongoConfig.java
@@ -1,4 +1,3 @@
-package com.b301.knpl.config;
-
-public class MongoConfig {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:0dd751274f8ccdccdbd915d1c89bf3a5efe406a2710850a3583e3d1eb02baf1b
+size 802
