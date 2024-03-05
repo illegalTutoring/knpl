f43b228417b9a07ef8173b0a2e91e0dad4a0c9a3
정종욱 <cnhug3@naver.com>
2024-03-05 14:21:10 +0900

    fix(router): ExView로 라우터 연결
    라우터 연결이 AboutView로 연결돼어 있어서 라우터 ExView로 연결 경로 수정.
    
    
    Former-commit-id: c9c7ddf50987938abf4235675aee78ec5df8b4e2

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ed0fe852aa71d4042c26a8ec6c0c3cb194123e5fc20ef1cab199fa5525d3f822
-size 587
+oid sha256:e38a2876d9e0a32266e359731cf522d834a2661a3048b8f5070ef7e515670613
+size 589
