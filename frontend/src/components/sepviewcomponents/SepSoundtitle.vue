2568c87c195ee742beeaa7efb7dd3f2712bd5afd
정종욱 <cnhug3@naver.com>
2024-03-06 13:08:48 +0900

    fix(SepSoundtitle): Css 수정 및 CSS 분리.
    외부 CSS로 분리완료.
    
    
    Former-commit-id: 42a15026aea6ce4e7196155a9eb9c2c2ee4bc74a

--- frontend/src/components/sepviewcomponents/SepSoundtitle.vue
+++ frontend/src/components/sepviewcomponents/SepSoundtitle.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8fc66dfba2e3995545ed4c2cf1bfa2f32580f6b543b2a48dcfe38bde8c891fc7
-size 2785
+oid sha256:2ef5352e8fc68d1106e7501e9341f29b47b7a7c1817b077dd91fcc42ea35fc92
+size 2244
