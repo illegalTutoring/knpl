082c4ab7152a3d1b0fc456e4a1d5d3696d2bc01d
정종욱 <cnhug3@naver.com>
2024-03-06 15:41:34 +0900

    feat(conversionview): 변환 화면 구성 시작.
    변환 화면 구성에 필요한 화면들 생성 및 변환 title 화면 구성
    
    
    Former-commit-id: 7b9149493b31abc339795aa736b8fe08792c71f2

---
+++ frontend/src/components/conversioncomponents/conversiontitle.vue
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b1dc994f3091c3a32d2b7e1102dd52d9c55479d7caa1076c7f1b081c21a72a7d
+size 2276
--- frontend/src/views/ConversionView.vue
+++ frontend/src/views/ConversionView.vue
@@ -1,9 +1,18 @@
 <template>
     <div>
-        음원 변환
+        <div v-if="!store.check">
+            <conversiontitle/>
+        </div>
+        <div v-else>
+            <conversion/>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
+import conversiontitle from '@/components/conversioncomponents/conversiontitle.vue';
+import conversion from '@/components/conversioncomponents/conversion.vue';
+import { useSeperateStore } from '@/stores/seperate'
 
+const store = useSeperateStore()
 </script>
