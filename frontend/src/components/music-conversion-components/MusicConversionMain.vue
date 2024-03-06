1a895ddec7bf841f2f29ce8b79dca456f19fc6aa
정종욱 <cnhug3@naver.com>
2024-03-07 01:27:42 +0900

    feat(conversion): 음악 변환 페이지 구현 완료
    MusicConversionMain.vue 화면 구현 완료.
    
    
    Former-commit-id: 951c87fec6892927429d6bdbf261051fad13e0d5

--- frontend/src/components/music-conversion-components/MusicConversionMain.vue
+++ frontend/src/components/music-conversion-components/MusicConversionMain.vue
@@ -1,7 +1,3 @@
-<template>
-    <div>
-    </div> 
-</template>
-
-<script setup lang="ts">
-</script>
+version https://git-lfs.github.com/spec/v1
+oid sha256:39ff951523999748effd002dc1abf52e02dff439adeb93bbdf30a86e792f07a4
+size 4574
