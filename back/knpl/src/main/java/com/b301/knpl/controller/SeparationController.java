c5b4d06d274f3a1de917efbb6a753218adb1f897
임혜령 <hyegpfud@gmail.com>
2024-03-07 17:31:00 +0900

    fix(svc,separation): Controller 내 RequestMapping 수정, 일부 오류 수정
    Former-commit-id: 09dd7567cb1246d74c1a428e5cbe93c8a1b05baf

--- back/knpl/src/main/java/com/b301/knpl/controller/SeparationController.java
+++ back/knpl/src/main/java/com/b301/knpl/controller/SeparationController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:763d4fa6c819c8a187c4608210bcae8d4ca08622302f42ac128d6a42754dc56b
-size 6631
+oid sha256:9e7a47ea499a9b14a3d0ebbca44ae8401cdffcc87956af8e540490d5cbe28e6f
+size 6553
