0f141ee5d01a89c48ce65a909192b3e4d5d73889
정종욱 <cnhug3@naver.com>
2024-03-07 17:13:26 +0900

    fix(image): 이미지 파일 경로 수정
    원인: npm run build 시 이미지가 일부만 빌드 되는 문제.
    결과: public 폴더로 이미지들을 옮겨서 해결. 이미지 사용되는 컴포넌트 src 경로 수정.
    
    
    Former-commit-id: b09113b46197972e11db477e364ece9e1ceb22a1

--- frontend/src/components/image-slide-components/ImageSlideComponent.vue
+++ frontend/src/components/image-slide-components/ImageSlideComponent.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4963ece16513c46d8c7ec5b3cfd68ea8973c3285c23dedf764a0276820ea29ce
-size 1382
+oid sha256:b808f9b9921bc1668ac511f48264de50aa6276fdf4a12b7c25eab575ba0ac88a
+size 1370
