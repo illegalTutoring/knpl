f3de828d2c298a73c3ca5db3f078e3969b8fbce5
Charles <target511@kakao.com>
2024-03-08 17:14:23 +0900

    feat(ddsp-api): SpringBoot API 통신과 추론의 비동기화
    SpringBoot로부터 변환, 분리 요청을 수신 후 요청 값이 정상적이라면 202를
    응답한다. 이후, AI추론 동작 및 완료파일을 요청에 담은 POST요청을
    SpringBoot로 송신할 수 있도록 프레임을 비동기적으로 구성하였다.
    
    
    Former-commit-id: 1d04ef5612db6d45011b7cb5391619740ad56ed9

--- ddsp/flask_api.py
+++ ddsp/flask_api.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44e6235ae785647293ba7d0604e5c47da7be2799e6093f6f190e99439ba7d5d8
-size 8464
+oid sha256:79a901cd90adb613e49bc5aa7bc6e9f55ab16d7fa00d3b0bc2c35c35d2caf560
+size 1919
