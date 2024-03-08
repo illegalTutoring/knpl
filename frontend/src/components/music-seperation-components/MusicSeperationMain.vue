2a131386c3013fa28bcb5a9b4337edf6e50b65ef
정종욱 <cnhug3@naver.com>
2024-03-08 11:08:34 +0900

    fix(musicseperation): 음원 분리 화면 크기 조정, 다중 음원 재생 함수 수정, 이퀄라이징 함수 수정
    외부 CSS 클래스 추가,
    멀티 트랙 생성 함수 수정
    이퀄라이징 오류 수정.
    
    
    Former-commit-id: 7564018315a60a186b4995e5ca817a763eb5e13b

--- frontend/src/components/music-seperation-components/MusicSeperationMain.vue
+++ frontend/src/components/music-seperation-components/MusicSeperationMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:81c56b7d03ba907a8aa6e496008a6e615257cdaea10db8454eda72f5b6d7fee8
-size 4127
+oid sha256:b421db8904ac703676eb8b89a57a11c1c8589f037db329a9f3f510a04a882d48
+size 6162
