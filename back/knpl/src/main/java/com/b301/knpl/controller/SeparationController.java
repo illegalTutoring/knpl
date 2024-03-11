2bf67729ef0a5c97c83b7907dc66715a2c624ac3
박예원 <parkyw0321@gmail.com>
2024-03-11 10:25:18 +0900

    fix(separation): api/separation 파라미터 변경
    Former-commit-id: cc3b32f0764d6161b08f0a75c876626907c4d27e

--- back/knpl/src/main/java/com/b301/knpl/controller/SeparationController.java
+++ back/knpl/src/main/java/com/b301/knpl/controller/SeparationController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:877fe00d0db697c7e5ed1cb8378c010608953028c71081c4b0a89bf5d4b8534b
-size 8672
+oid sha256:b66a49b409aa24bccabc117b8cb7fc8b6a5aece6523a8acbacd25858bfdc4409
+size 8710
