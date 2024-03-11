cfdc7212770ed2b6385d4154b5f053083d4ffa06
Charles <target511@kakao.com>
2024-03-11 14:35:48 +0900

    feat(celery): BE로부터의 요청에 대한 결과 데이터를 '동기' 방식으로 전송
    BE로부터 svc 요청을 수신하고 요청을 잘 수신했다는 응답을 보낸뒤
    Celery를 통해 추론 과정을 비동기로 수행한다.
    이때 추론 결과를 다시 BE로 보내주게되는데 이 통신을 '동기' 방식으로 REST
    요청을 보낸다.
    주의) Celery내에서 비동기 요청을 보내는 것은 권장되지 않는다. 비동기
    작업을 실행하기 위해서는 이벤트 루프가 필요한데 Celery task 내에서
    직접적으로 새로운 이벤트 루프를 관리하는 것은 예상치 못한 동작이나
    충돌을 일으킬 수 있으며 성능문제를 발생시킬 수도 있기 때문이다.
    
    
    Former-commit-id: 81af909bb7e6b69b9cd611706baca0c67b4111be

--- ddsp/flask_api.py
+++ ddsp/flask_api.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:79a901cd90adb613e49bc5aa7bc6e9f55ab16d7fa00d3b0bc2c35c35d2caf560
-size 1919
+oid sha256:05b7b956c98664e3a1cbbaf120491436c519eb4763ce9f067dd4ae8173b8d0f3
+size 1920
--- ddsp/tasks.py
+++ ddsp/tasks.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5b23958dbb6da085eb33d329f36e12b2bdbc3147e9bce85632fd5ae89c4ed620
-size 7021
+oid sha256:e98200ea265b63bf9d1f5d8349c9649f259eeba473852e96cdeb30f17b8d95be
+size 6710
