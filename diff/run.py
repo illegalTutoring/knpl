fb8cafa5f9732b85e0800e92a63d94704c829bba
조영진 <tl5235566@gmail.com>
2024-03-11 14:01:01 +0900

    feat(async API): diff api서버 비동기 동작 처리
    Former-commit-id: a9e5d22a554e6f296b7f134e1f0c79e912d33802

--- diff/run.py
+++ diff/run.py
@@ -1,16 +1,3 @@
-import importlib
-import os
-from utils.hparams import set_hparams, hparams
-set_hparams(print_hparams=False)
-
-def run_task():
-    assert hparams['task_cls'] != ''
-    pkg = ".".join(hparams["task_cls"].split(".")[:-1])
-    cls_name = hparams["task_cls"].split(".")[-1]
-    task_cls = getattr(importlib.import_module(pkg), cls_name)
-    task_cls.start()
-
-
-if __name__ == '__main__':
-    run_task()
-
+version https://git-lfs.github.com/spec/v1
+oid sha256:c8bacc47e4e993926f6c71de33dfe24581ce2681f1c76349798eaf5116ac9f13
+size 396
---
+++ diff/tasks.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:06128765be0e3557f1b55ff4bff76011cbb28fbe1ef40bd09d9af63fdcb39453
+size 7190
