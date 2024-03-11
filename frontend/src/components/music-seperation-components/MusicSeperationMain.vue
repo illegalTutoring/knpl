d0f03d180e300965d5b4311c180982718942f683
정종욱 <cnhug3@naver.com>
2024-03-11 15:35:51 +0900

    feat(sepview): 트랙별 소리 음소거 버튼 추가, 분리 타이틀 화면 CSS 작업.
    음악 분리 메인 화면 소리 뮤트 로직 추가 및 테스트 확인
    음악 분리 타이틀 화면 CSS 작업 1차 완료.
    
    
    Former-commit-id: d156bd39f5e2a0e7e3bd6713674c5d3423e72a63

--- frontend/src/components/music-seperation-components/MusicSeperationMain.vue
+++ frontend/src/components/music-seperation-components/MusicSeperationMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:80a9b89c5ff1e7612ada1cb50f6029c3f3f7e9bb4801d88eff5e85d3f74a7543
-size 7078
+oid sha256:764b8886e366e1fa7b7e6c8cd68df2f15c52f49fb1fa5b67ae60a040bb6661b3
+size 8395
--- frontend/src/components/music-seperation-components/MusicSeperationTitle.vue
+++ frontend/src/components/music-seperation-components/MusicSeperationTitle.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d45897cf05a41070d0cc23dd65ed4a00cfe4327d420ee71c60620cf838275df0
-size 2519
+oid sha256:79dea19427974b1625d509f082fa7dc89eba8be367efe6c31e987866f02deacf
+size 3070
