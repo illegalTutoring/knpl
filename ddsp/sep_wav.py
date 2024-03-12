999090de2da0ebc93a342aab752f53df14dac237
Charles <target511@kakao.com>
2024-03-12 12:25:11 +0900

    feat(demucs): 특정 파일만 음원 분리 할 수 있는 기능 추가
    기본 demucs는 지정 디렉토리 내 모든 wav파일을 적용한다.
    demucs를 오버로딩해서 파라미터로 받은 특정 파일만 적용할 수 있도록
    수정했다.
    
    
    Former-commit-id: 4537cf6b183e88c878432350a871d53d72c8a3ba

--- ddsp/sep_wav.py
+++ ddsp/sep_wav.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fffa118a32e063185d0281d855a4e253c0ea96065c42cda1d14069d142f9e886
-size 12717
+oid sha256:518b79f5d5e4dd86ba7b7775a030d10606eb1d0903521a404e1e218fed8a39c0
+size 14829
