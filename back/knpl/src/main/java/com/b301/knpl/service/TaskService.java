81d81881f3baaded60cec993cdd4fd5d5d5a84be
박예원 <parkyw0321@gmail.com>
2024-03-12 13:39:16 +0900

    feat(Task): Task, TaskRepo, TaskService 작성
    Former-commit-id: 657aab69347c752726c07a7d9bd653b52c62fc06

--- back/knpl/src/main/java/com/b301/knpl/service/TaskService.java
+++ back/knpl/src/main/java/com/b301/knpl/service/TaskService.java
@@ -1,4 +1,22 @@
 package com.b301.knpl.service;
 
+import com.b301.knpl.entity.Task;
+import com.b301.knpl.repository.FileRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+//@Service
+@Slf4j
+@RequiredArgsConstructor
 public class TaskService {
+
+    private final FileRepository fileRepository;
+    private final TaskService taskService;
+
+    public Task insertTask(String orignal, String outputEx){
+
+
+        return null;
+    }
 }
