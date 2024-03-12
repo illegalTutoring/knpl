7118369d852c8c29c960d1bedf29f6fc5207b551
Charles <target511@kakao.com>
2024-03-12 13:43:58 +0900

    feat(separation): 음원 분리 및 변환 수행 후 BE로 completion 요청 전송
    Former-commit-id: 38c500aa9fd42151f3f711147af96d6c5a75d14e

--- ddsp/flask_api.py
+++ ddsp/flask_api.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d423bdb406d28edf248018d26c1d253f85ef045e5c04fdcb9e4ffb17c35d2cf
-size 2549
+oid sha256:e4048399cf070710acdda630a3e0369f0faf60767186b0d872aefa0dbb1787a9
+size 2574
--- ddsp/tasks.py
+++ ddsp/tasks.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10553e9acf9712cbd973e276b833ec20983976764382abb09ad9f976b70dc708
-size 8324
+oid sha256:27c1f591df06d766664bd8735453f2256d836d42dcd6fd66b29daf3ebf4be137
+size 9411
