28f671abbc35c32a0d68b3a665d7d775d6cecd9b
정종욱 <cnhug3@naver.com>
2024-03-14 01:46:05 +0900

    feat(transition): 음원입력, 음원변환, 홈화면 컴포넌트 트랜지션 적용
    !:메뉴얼 화면은 아직 컴포넌트가 작성되지 않아 해당 화면은 트랜지션
    적용을 하지 않음.
    
    
    Former-commit-id: d4233356dae160beccee3178d075f51b2b01d4d0

--- frontend/src/components/music-input/MusicInputComplete.vue
+++ frontend/src/components/music-input/MusicInputComplete.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3421f94098abe03cc2244a0cd3e848de1a78d154f546c69fe0d20324c0491896
-size 2121
+oid sha256:d6dc44ab561817ac0fed8b5b2738e6c2f60ef6116e3346350821aa3923d76afd
+size 2349
