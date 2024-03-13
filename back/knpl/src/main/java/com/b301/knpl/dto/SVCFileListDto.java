ef5daf52a3e326c4db94d2de754ab6d451c0ca49
임혜령 <hyegpfud@gmail.com>
2024-03-13 12:36:45 +0900

    feat(svc): SVCInfo, SVCFileLIst Dto 추가
    Former-commit-id: af7ec9cc189116d88244a865792ddb52e121bffa

--- back/knpl/src/main/java/com/b301/knpl/dto/SVCFileListDto.java
+++ back/knpl/src/main/java/com/b301/knpl/dto/SVCFileListDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3cece5fe7fd0a377007558e0b6335c2c3c4d418902a3154fc6648e3ad8c99aa1
-size 358
+oid sha256:ee47971f497ebc6261a4a715ed17ac6f26b836b0c1f29804ae94c78493e2a947
+size 433
