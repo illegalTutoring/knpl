1fe44889e803a729a3adffb2bc20b08a5d4380ee
정종욱 <cnhug3@naver.com>
2024-03-13 13:04:54 +0900

    feat(imageslide): 이미지 슬라이드 제작 완료
    (!): 배경이미지는 추후jpg 혹은 GIF로 수정예정.
    (!): 아직 이미지 슬라이드 다크모드는 적용 안함.
    
    
    Former-commit-id: 49a1b004474a438513167da6ca164a774f9052f8

--- frontend/src/components/image-slide-components/ImageSlideComponent.vue
+++ frontend/src/components/image-slide-components/ImageSlideComponent.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22e8069afdc7c97b8c484d051e92dbf0aa11274b36e3218f01e3ceb1b5d97857
-size 1729
+oid sha256:82c3bb7a7c26cc82178f2cca6fa63e8652cf19f17efa674e88830e34cd09f2a0
+size 3104
