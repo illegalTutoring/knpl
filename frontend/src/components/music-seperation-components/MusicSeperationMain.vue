76eb4e829b0f99c462e087f478de202d7c6196b5
정종욱 <cnhug3@naver.com>
2024-03-13 16:21:04 +0900

    fix(musicseperation): 타이틀 화면 내부요소들  드래그앤 드롭 이벤트 막기.
    (!): CSS 추가 조정 필요.
    
    
    Former-commit-id: 82ce0ff97840a87c1b546c4bac81aa6b3694482c

--- frontend/src/components/music-seperation-components/MusicSeperationMain.vue
+++ frontend/src/components/music-seperation-components/MusicSeperationMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:764b8886e366e1fa7b7e6c8cd68df2f15c52f49fb1fa5b67ae60a040bb6661b3
-size 8395
+oid sha256:501ef06a192b85599e37a128f9261d762d074ed71bec306473ea1d581dc35c1e
+size 8513
