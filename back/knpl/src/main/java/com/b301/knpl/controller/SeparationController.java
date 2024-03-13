a63299d0553b191fa90afe1b627f63f5792ee95e
박예원 <parkyw0321@gmail.com>
2024-03-13 14:44:31 +0900

    fix(separation):  controller 수정
    Former-commit-id: 661fa72a15aa08cb08241ff9899edadf3a4b56b7

--- back/knpl/src/main/java/com/b301/knpl/controller/SeparationController.java
+++ back/knpl/src/main/java/com/b301/knpl/controller/SeparationController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b66a49b409aa24bccabc117b8cb7fc8b6a5aece6523a8acbacd25858bfdc4409
-size 8710
+oid sha256:6d5ffc3cfde1c181e4273da34d76acff66afa866a125dd128150108916a63981
+size 10980
