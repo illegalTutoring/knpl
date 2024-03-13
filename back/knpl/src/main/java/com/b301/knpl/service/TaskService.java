a0d740bf32e76e954c47e148b1e1884519bed6e9
박예원 <parkyw0321@gmail.com>
2024-03-13 15:58:16 +0900

    fix(task):  task 엔터티와 taskService 수정
    Former-commit-id: 7f85e8b68f1980ca291a8f70d9e913556c6228fd

--- back/knpl/src/main/java/com/b301/knpl/service/TaskService.java
+++ back/knpl/src/main/java/com/b301/knpl/service/TaskService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aabbe5bd76ac12c098c4e6b1ef7407db5c3a300bc09ae5a02c4be546fa9ed0f9
-size 1073
+oid sha256:cfcba6216e67317ed4f41289c1257ce53826ba9c7b444ddde8de84ee9c82d3ba
+size 1575
