0b8d27179151701c68d1b3475e1fac273744e850
박예원 <parkyw0321@gmail.com>
2024-03-14 13:31:22 +0900

    fix(separation): fileUpload 시 프론트에 taskId 전달 추가
    Former-commit-id: ebc9a7a82dadead89f05c9004681c753677461c5

--- back/knpl/src/main/java/com/b301/knpl/controller/SeparationController.java
+++ back/knpl/src/main/java/com/b301/knpl/controller/SeparationController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:045fe4d3ed70cc56ec8e26bb76540cb9d0c4e0037933c19f08fce149f8092dcf
-size 11862
+oid sha256:d1f77c7694e4f5d3fcf2ca8c2aee75f3d3b2bdd374229faf0524f3cebca5c4ba
+size 12898
