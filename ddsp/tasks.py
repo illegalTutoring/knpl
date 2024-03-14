41b8e585c345e7f69862279fca5d5f4af229b393
Charles <target511@kakao.com>
2024-03-14 11:26:18 +0900

    feat(svc_separation): 분리된 파일을 변환하는 API 기능
    로컬 테스트 완료. BE와 직접 통신해보는 테스트 필요.
    
    
    Former-commit-id: e69763e58b1822a1ce52155f3709cc1949b88d28

--- ddsp/tasks.py
+++ ddsp/tasks.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:06844b7d9aadc1e5706c115f4f62d6f0b5ad42a1d8a9ccebe5a64f3f413e32e2
-size 12197
+oid sha256:502bd2ba1e13a1ef6a23cdfe3b28b2bb74b191a2cde4512f29592306898d972d
+size 18055
