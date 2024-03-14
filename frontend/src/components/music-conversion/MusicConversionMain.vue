2e4e8e53e97d3ad9ecb5b3f743c29a7cf2ddd9f5
정종욱 <cnhug3@naver.com>
2024-03-15 00:02:49 +0900

    fix(title): 음원 변환, AI 커버, 커버 결과 타이틀 변경.
    음원 변환 => AI 커버
    AI 커버 => AI 커스텀
    커버 결과 => 커스텀 결과
    
    
    Former-commit-id: 13a7c9cb634f177574f8f08282d4b9da58b57d91

--- frontend/src/components/music-conversion/MusicConversionMain.vue
+++ frontend/src/components/music-conversion/MusicConversionMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ec3947d8b7b651bf03e7c859a1db9b8e7061b9fa8cc9146f6971154e239545e
-size 3752
+oid sha256:61feb7fe9d6d9e66034ed17a4aaa295cfd5befc5584c545fd4c0c7098c4ad929
+size 3748
--- frontend/src/components/music-input/MusicInputMain.vue
+++ frontend/src/components/music-input/MusicInputMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab06f3ada04eed22af9f90a7b7474cd55ad40878856d65fd8b02221af432e456
-size 5057
+oid sha256:0f88d6f46c6442d14aeb3540130b29913b19f5cfcb31784b93395692d98bc9ba
+size 5060
