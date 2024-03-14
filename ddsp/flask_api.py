339c0d71f172cb0db808184259a0d86685f8031d
Charles <target511@kakao.com>
2024-03-14 11:26:18 +0900

    feat(svc_separation): 분리된 파일을 변환하는 API 기능
    로컬 테스트 완료. BE와 직접 통신해보는 테스트 필요.
    
    
    Former-commit-id: 7471ec86b4d04e345f5efc80b130c0fa2f808192

--- ddsp/flask_api.py
+++ ddsp/flask_api.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d1404746ab640522821a3e9b14e326c3830e79b62f7b47d004753cd1b83bd24
-size 3645
+oid sha256:827a665e0dfd9af9e0d0ba9d76aeefa24585f158f7d6bdfdfb811d4847275b0f
+size 4770
