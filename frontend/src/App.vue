106fce329a4cba56cfb2d42e7700a66d91400f69
김태희 <apples5060@naver.com>
2024-03-16 21:53:27 +0900

    docs(infra): 인프라 테스트 및 Jenkins CI/CD 구축 완료
    1. 인프라 테스트 및 버그리포트 작성
    2. Jenkins 프론트/백 엔드 CI/CD pipeline 작성 완료
    
    
    Former-commit-id: 2c8dfd08ebe15955766be9cddabd9f8b3f0c5aeb

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,18 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4ff48a693cddc93d3a46245908a5557a58af75c2e1a923d57a9fc2a3cc9d1f1c
-size 295
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import Navbar from '@/components/navbar-components/Navbar.vue';
+
+</script>
+
+<template>
+    <div>
+        <div>
+            <Navbar />
+        </div>
+
+        <div>
+            <RouterView />
+        </div>
+    </div>
+</template>
+
