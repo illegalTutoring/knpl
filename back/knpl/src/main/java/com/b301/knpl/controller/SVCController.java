07c1b7a7601f9c9f0bdc0fbf13163800a186186c
임혜령 <hyegpfud@gmail.com>
2024-03-18 17:51:21 +0900

    feat(svc): startSVCWithSep 수정, createSVCWithSepRequest 생성 추가
    Former-commit-id: c6fdfd4ea96e57ef83df1996cd862dd61f3ce630

--- back/knpl/src/main/java/com/b301/knpl/controller/SVCController.java
+++ back/knpl/src/main/java/com/b301/knpl/controller/SVCController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b5a88054d05bbc48b573bd1bfaf1b401ab8cef6f9767fb7f71e05c0ed0c75aaa
-size 15760
+oid sha256:83efc619587de1a10ed12c70797bdc36200f6bf7d90c0566914ecc941c76bcd5
+size 15647
--- back/knpl/src/main/java/com/b301/knpl/service/WebClientService.java
+++ back/knpl/src/main/java/com/b301/knpl/service/WebClientService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:742b5035f66a6d1ce572eb5796ebf8dd07690fdf68382803a758c89e6908f602
-size 5187
+oid sha256:9497807e7fd75622b930a558b00a96828ca08de48de9fc71644aca1a3ff1a41b
+size 6436
