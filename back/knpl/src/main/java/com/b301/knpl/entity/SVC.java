b0c311504c335bec69204004d6b0360b14cd5dec
임혜령 <hyegpfud@gmail.com>
2024-03-19 16:38:59 +0900

    feat(svc): 변환 후 분리 기능 개발
    Former-commit-id: 006462ed0032cbd4b6ff0bbb1370dbaf767753cd

--- back/knpl/src/main/java/com/b301/knpl/entity/SVC.java
+++ back/knpl/src/main/java/com/b301/knpl/entity/SVC.java
@@ -1,3 +1,15 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d65bd873fbeb863395bd24f268535a0486306d80690739d575c660431b39cee4
-size 298
+package com.b301.knpl.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class SVC {
+    private String file;
+
+    // 생성자, 게터, 세터 등 필요한 메서드들을 추가할 수 있습니다.
+    @Builder
+    public SVC(String file) {
+        this.file = file;
+    }
+}
