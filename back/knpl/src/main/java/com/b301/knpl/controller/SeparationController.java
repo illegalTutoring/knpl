d6914b3fdd3762c449895608c87e736188b33626
박예원 <parkyw0321@gmail.com>
2024-03-19 11:06:13 +0900

    feat(Separation): separation 데이터 get 요청 controller, service 작성
    Former-commit-id: 6746a0ab356ef19a1b872dc1e4946a2be0e92b27

--- back/knpl/src/main/java/com/b301/knpl/controller/SeparationController.java
+++ back/knpl/src/main/java/com/b301/knpl/controller/SeparationController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b342f7079e5a978d153c2df2ecd574e6513bc84e00dccab9038d4e8f83e99742
-size 16381
+oid sha256:7b790af69ad880bd2191de02638ee5ee002a7bf52d1ffdcba30fec35a7792948
+size 16739
