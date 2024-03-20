3982e5e738b74b9c8b4e5d8484a4cf193cda739a
임혜령 <hyegpfud@gmail.com>
2024-03-20 17:54:49 +0900

    feat(svc): api 추가에 따른 일부 메소드 수정 및 api 개발
    Former-commit-id: 516e2b93fda7b013e432c9b43efb3ada7eebdcee

--- back/knpl/src/main/java/com/b301/knpl/controller/SVCController.java
+++ back/knpl/src/main/java/com/b301/knpl/controller/SVCController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a3b879c8115940eab2c35c51cd09548b30ff01bc76d3923cec80e4249510a5f6
-size 15426
+oid sha256:df528ed12ed2061dfeb22718401b4a727819b057a6ee34391c3ddfa936adc4eb
+size 20028
