835ad5a7182ab89759f9ce0fd8e87c2ef2bd452e
정종욱 <cnhug3@naver.com>
2024-03-20 22:45:52 +0900

    feat(musicinput): musicinput.ts 생성
    해당 스크립트 생성으로 musicinputView관련 컴포넌트 변수 수정.
    
    
    Former-commit-id: a545c958361129b2748ee48da0073939181cf3f8

--- frontend/src/components/music-input/MusicInputMain.vue
+++ frontend/src/components/music-input/MusicInputMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0f88d6f46c6442d14aeb3540130b29913b19f5cfcb31784b93395692d98bc9ba
-size 5060
+oid sha256:2c9327ff267689dbc81c31609336ee2ae1f962ddd676c7f6c56de75998d95ce2
+size 5066
