a738e05c1c2814c1a070e905f0360381f8691def
조영진 <tl5235566@gmail.com>
2024-03-20 17:31:18 +0900

    feat(custom): api/diff/completion 구현중
    - MongoDB의 필요성 재고
    - token값을 사용하여 파일을 찾게할지, MongoDB의 고유 ID를 통해 파일을
      찾게할지
    - DIFFController 내부 불필요 함수 Service로 이
    
    
    Former-commit-id: 81239dae7b29803829d98a9ee7df00f8dbaaef63

--- back/knpl/src/main/java/com/b301/knpl/entity/Custom.java
+++ back/knpl/src/main/java/com/b301/knpl/entity/Custom.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3cccf00a7f459565e2676d2abeec1625a605f709f99e669cf58906dc5844f680
-size 912
+oid sha256:11671716c18cccb47e99c478b8284a4990652dadda76f22468b8ea2fe255a686
+size 619
--- back/knpl/src/main/java/com/b301/knpl/repository/CustomRepositoryImpl.java
+++ back/knpl/src/main/java/com/b301/knpl/repository/CustomRepositoryImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:49dd1fb30252366ab22925f9720292e557b3e4b4c67d9721667b3cfe9fbf4bd7
-size 1351
+oid sha256:cacae152fd1b3bad7ff8a1788877fc7a740080a7c7c146ddb65d92c0c0dd40dd
+size 1426
