4d2189e5f30d5d0eb82298747fbbd0360ca2dc2f
오철수 <target511@kakao.com>
2024-03-20 11:06:23 +0900

    feat(mixing): Raw Json 타입의 데이터를 받을 수 있도록 기능 추가
    Former-commit-id: fe4c7b80209eb217873ef3b5833b2d29db3f4791

--- ddsp/tasks.py
+++ ddsp/tasks.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:502bd2ba1e13a1ef6a23cdfe3b28b2bb74b191a2cde4512f29592306898d972d
-size 18055
+oid sha256:0f699e95f575fd6176d0a5e6483bd7979aaed8dce546fb329ae282c06aa52dfc
+size 18099
