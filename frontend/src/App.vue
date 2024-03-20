367797101fd52d7cfcc8f36fd83d19bba79893cd
정종욱 <cnhug3@naver.com>
2024-03-20 23:25:21 +0900

    fix(exitalarm): exitalarm 출력조건 수정
    exit-alarm.ts에서 필요없는 변수 제거
    
    
    Former-commit-id: 56d4c149faf4a248a67f26b995e40a1e3f136c06

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,14 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b914ddd08aa34d3cdcbb64b6a05b9db639f7dcadb4f8ff44c0d44223d753bebb
-size 179
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+
+
+</script>
+
+<template>
+    <div>
+        <div>
+            <RouterView />
+        </div>
+    </div>
+</template>
+
