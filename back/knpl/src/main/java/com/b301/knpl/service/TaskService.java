accaa55d92b0657f9221938b745c62c3e7e72455
박예원 <parkyw0321@gmail.com>
2024-03-20 14:38:33 +0900

    feat(task): updateMix 함수 추가
    Former-commit-id: 2618c8f3bf92bccbd660f0e993394fc6e9243b10

--- back/knpl/src/main/java/com/b301/knpl/service/TaskService.java
+++ back/knpl/src/main/java/com/b301/knpl/service/TaskService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c836370d37a627f78ee8af992b67ff13070d8eb4ec6811fbf2ae49dd147bb763
-size 2222
+oid sha256:6c7da34a3823abbec3a6870cb69950dde0f32850e509f702ab74ac4ea0ef6d64
+size 2768
