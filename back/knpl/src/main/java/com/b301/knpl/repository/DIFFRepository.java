d4c0e002a259b902fcf0fb601374ca23293c6b2f
조영진 <tl5235566@gmail.com>
2024-03-21 13:21:28 +0900

    feat(custom): custom api completion 작성
    - Security 적용 가능성 염두 -> 적용 안하기로 결정
    - MongoDB는 사용하기로 결정
    - 파일 입출력 구현 완료
    
    
    Former-commit-id: 7bc79592a57fabb5c006998bc31110358ebe425e

---
+++ back/knpl/src/main/java/com/b301/knpl/repository/DIFFRepository.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e0a1e9432ffca67c99ea136b076befde49872cbe96ffe3bdb3f42e2bf7cf59e4
+size 460
--- back/knpl/src/main/java/com/b301/knpl/service/CustomService.java
+++ back/knpl/src/main/java/com/b301/knpl/service/CustomService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a72931ef16da2a85c1d9e31c229058ccbfb485866d3d13b3709cb61b14218001
-size 2390
+oid sha256:d4c8f288fe141aa4bb7532628ce7bd9038d1d385e56e3249e0b356203a9b5c7c
+size 2604
