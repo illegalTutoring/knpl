ab31ce99322316b4cf3dd96af4e54380a45beb92
정종욱 <cnhug3@naver.com>
2024-03-21 09:49:58 +0900

    feat(loading): loading화면 CSS 적용
    사용하지 않은 사진들 같이 삭제 조치.
    
    
    Former-commit-id: 585c9aa63d40b9eb59a750ddb82acaf86abc43c3

--- frontend/src/components/loading/Loading.vue
+++ frontend/src/components/loading/Loading.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:edadedae0192f042999c31f0da25838f89d7edea4b5da36a670f3f4e07adb870
-size 706
+oid sha256:6f6df25852aa25f9987071f410813bec210b815af82facb11926c887ec549199
+size 1757
