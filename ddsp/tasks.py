591fdf6efcac0e7b8041d427ae658d2920a95c68
오철수 <target511@kakao.com>
2024-03-21 16:08:13 +0900

    feat(svc_separate): 분리된 파일을 svc 할 때 해당 파일들도 저장되는 기능
    Former-commit-id: 7a5791d914cfe68d5408e5f17bea280ff0ae19ba

--- ddsp/tasks.py
+++ ddsp/tasks.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1df456365c5f683d7244eab9519a664aabbcf64780ad5d061f6399999bd1cacd
-size 21224
+oid sha256:59c69737b136dbcf758787345daa63925f00f93d737326a72e98e0ab662e79a1
+size 21095
