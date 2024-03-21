16bb4e136ec8ddbe1e6e3982b8307c3b64c3a7fa
조영진 <tl5235566@gmail.com>
2024-03-21 16:09:29 +0900

    feat(custom): custom api 작성 완료
    Former-commit-id: c9954c56db1eb112152670b7a2c9463b38a34399

--- back/knpl/src/main/java/com/b301/knpl/repository/DIFFRepository.java
+++ back/knpl/src/main/java/com/b301/knpl/repository/DIFFRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e0a1e9432ffca67c99ea136b076befde49872cbe96ffe3bdb3f42e2bf7cf59e4
-size 460
+oid sha256:8d86b093a28ae6fcd7ca9a2a50e27ed6e7c8c2112c9145cb4ef8fc7310d550e0
+size 505
--- back/knpl/src/main/java/com/b301/knpl/repository/DIFFRepositoryImpl.java
+++ back/knpl/src/main/java/com/b301/knpl/repository/DIFFRepositoryImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:872ac2bca2c596171c339b55212b6a68b149c5f06749bdb53195f061c60f938f
-size 1395
+oid sha256:89cd17b5e32de3cc5ac5ab97702efad7a38b07fc71d9d1e7b4b89c8952076ed7
+size 1595
