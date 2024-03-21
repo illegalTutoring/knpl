caf24b58cc0ab434cb668f54c8bfe3a7a0df9141
정종욱 <cnhug3@naver.com>
2024-03-21 09:54:53 +0900

    fix(exitalarm): exitResult 함수로직 수정.
    결과화면에서 메인화면으로 단순 나가기가 불가능한 오류 확인.
    해당 오류 수정 완료.
    
    
    Former-commit-id: 3060028b3ff41e67e7d6d705fd27e9b46e7667f5

--- frontend/src/stores/exit-alarm.ts
+++ frontend/src/stores/exit-alarm.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:81c320d6a53d3afaf76b43b7b88cb27d8563318ba8019aee767d1c940f9029ce
-size 554
+oid sha256:65801064dba62da3df52c3fa8a6b2d6db3e5e08c70b83f5d1db6e821762a91c6
+size 809
