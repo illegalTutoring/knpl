0548bb816bb9d7427e97bd74ab90fce188d4daea
정종욱 <cnhug3@naver.com>
2024-03-22 15:21:00 +0900

    Update seperate.ts
    Former-commit-id: e7a66a6bd1c8f347700a258f5bc460f94049f72f

--- frontend/src/stores/seperate.ts
+++ frontend/src/stores/seperate.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:298cd353e1764166aaee747dd505224cf22aac926e4ad2d43d40b88ac310a51b
-size 6159
+oid sha256:d8aaefb2dcedbf226e0ac685f28d8eba8e398d53eecbd0f638d81bf3ed982140
+size 6136
