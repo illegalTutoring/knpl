a5f56b80a25ed0d8822ab9790603c6f86e4f5c81
정종욱 <cnhug3@naver.com>
2024-03-22 12:44:55 +0900

    feat(separateapi): 분리된 음성 전달하기 api 생성.
    Former-commit-id: 7d86f174120652ad34cdaa7c10cc2da13c40b88f

--- frontend/src/stores/seperate.ts
+++ frontend/src/stores/seperate.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50689fcc97b5055189d787651a7afe46458cb3cd782d3185f326ce5a80a54b37
-size 3892
+oid sha256:32ff64bb52b0e002b44f883fa6145e86075a3ac5cefc1ca03d83e9f107db3969
+size 4403
