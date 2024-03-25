d12bf1555319354c658e69f80537bf16c6326a22
정종욱 <cnhug3@naver.com>
2024-03-25 15:11:41 +0900

    feat(api): 본인 목소리, 음원 합성 결과 확인 api 작성 완료.
    Former-commit-id: 69f63847705d2fd7c9e83ca24ddbe2c12e5acac5

--- frontend/src/stores/musicinput.ts
+++ frontend/src/stores/musicinput.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:95c56b3d6d8f5f86031445cdf23332817448453afaa3938ecf3c87e5541c28ac
-size 1872
+oid sha256:7b5b4f3e1edc7a3631ea1b2116b3906c0fdcb22c1d6f1c424bcbf01269bf874f
+size 2921
