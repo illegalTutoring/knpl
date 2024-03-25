42e03cb04cdfc3f3543630bdf6437abe5ee143ae
정종욱 <cnhug3@naver.com>
2024-03-25 13:45:50 +0900

    fix(api): computed 삭제, 변환된 음성 전달하기 수정.
    import {~, computed} from 'vue' 삭제.
    변환된 음성 전달하기 .catch(err) err 타입 추가.
    해당 api console.log(err) 제거.
    
    
    Former-commit-id: 3409b63abd327eb604f647470177f234bedaba4c

--- frontend/src/stores/conversion.ts
+++ frontend/src/stores/conversion.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ae802c76f95784a2fbc53fe2abda026eb7a4441596814b2015d9f58f29526048
-size 2756
+oid sha256:a71ce730764944c5a63af68140751d09e409db6f1c172ca4732f048eb69e8f92
+size 2721
