5ebd2949eacd196ec6a901162cdedb441aed33e4
박예원 <parkyw0321@gmail.com>
2024-03-25 17:22:34 +0900

    fix(separation): controller, service sonarqube 문제점 해결
    Former-commit-id: 378d8aa368e6cfc932e61e6bf8813db82b667014

--- back/knpl/src/main/java/com/b301/knpl/controller/SeparationController.java
+++ back/knpl/src/main/java/com/b301/knpl/controller/SeparationController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c45a8492d15f1c64b9aadec98609bc4a7303e3cbf89283ec722fa335ba4e6ea
+oid sha256:01d8267be7a6da4f3fbd5f98540dca7625d39aff1e66207222c90c48ee401924
 size 18116
