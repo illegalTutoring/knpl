ceea700e31f9f4b6dff9546afe2d75e5606802c6
정종욱 <cnhug3@naver.com>
2024-03-25 12:36:34 +0900

    fix(api): 변환된 음성 전달 api
    res 응답시 특정 변수에 필요한 data값 저장.
    settimeout 제거.
    
    
    Former-commit-id: bcf4d1b54539d3663154bded14bcbfb0b0cc84ee

--- frontend/src/stores/conversion.ts
+++ frontend/src/stores/conversion.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5351a98ca8707b76cc641ada437c2751e7997f5fbaf266ace995eaf6143ccfc
-size 2724
+oid sha256:ae802c76f95784a2fbc53fe2abda026eb7a4441596814b2015d9f58f29526048
+size 2756
