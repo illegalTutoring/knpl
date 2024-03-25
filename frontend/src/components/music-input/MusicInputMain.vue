56566b6c43f107a5145116dfb91bdf0a8f6dacd4
정종욱 <cnhug3@naver.com>
2024-03-25 14:43:32 +0900

    fix(mainView): 버튼클릭시 musicCustom 함수 작동 및 해당 vue에 필요 하지 않은 변수들 삭제 조치.
    Former-commit-id: 61b67f56222fcf7f08c9605ea8a710f2f6a57a1e

--- frontend/src/components/music-input/MusicInputMain.vue
+++ frontend/src/components/music-input/MusicInputMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1090db26f5cfc4ffea30baaeea9aa4cc3886706816a0b60f8b2ad3558aadcea
-size 5018
+oid sha256:1a4158c0c8b780b13ec3a018ef8fa0892f055b5be05d06f6c57011b5e7d3a4d4
+size 4689
