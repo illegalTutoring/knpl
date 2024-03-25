ee4f494ef0f3ef0ed60710d539c767ae5fa4a59a
정종욱 <cnhug3@naver.com>
2024-03-25 14:37:14 +0900

    fix(api): status 코드 숫자에따른 console.log출력 내용 변경.
    현재 console.log만 진행  추후 화면에 해당 문구 출력 예정.
    
    
    Former-commit-id: 41228c8c3f627d7d56e300c98180ec933fdaa661

--- frontend/src/stores/musicinput.ts
+++ frontend/src/stores/musicinput.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5159c82701e15d97404db8ca76dbf6c6e56d9ed7469da6950ca8bc3189e2f751
-size 1254
+oid sha256:95c56b3d6d8f5f86031445cdf23332817448453afaa3938ecf3c87e5541c28ac
+size 1872
