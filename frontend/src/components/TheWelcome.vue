338d6671b565a52ed7f2258c3ca2dc138295d623
박예원 <parkyw0321@gmail.com>
2024-03-25 10:45:56 +0900

    fix(separation): Controller message, Service 내 warning 리팩토링
    Former-commit-id: 7fc79845ef89e57951ad81de113b3612e1f0a857

--- frontend/src/components/TheWelcome.vue
+++ frontend/src/components/TheWelcome.vue
@@ -1,9 +1,3 @@
-<script setup lang="ts">
-import ImageSlide from './image-slide/ImageSlide.vue';
-</script>
-
-<template>
-    <div>
-		<ImageSlide/>
-    </div>
-</template>
+version https://git-lfs.github.com/spec/v1
+oid sha256:ed5defcc043390fe80d3bc5e0b312a5631de2e56a7f1c8a0458cd29900722390
+size 151
