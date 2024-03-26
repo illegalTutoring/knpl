4cf1b292e88b22df701f6b9168aea0c99bd8b573
임혜령 <hyegpfud@gmail.com>
2024-03-26 15:34:29 +0900

    fix(webclientservice): app.ai.baseurl -> app.ai.api2url로 이름 변경
    Former-commit-id: c41484d4a2fe76cd4b47a46916470433c2b89171

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,3 +1,9 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:aeebe1503cf2e44c730ba503e1f3fbe116db5cf909aaffdd8ac092cec52becc5
-size 159
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+    <main>
+        <TheWelcome />
+    </main>
+</template>
