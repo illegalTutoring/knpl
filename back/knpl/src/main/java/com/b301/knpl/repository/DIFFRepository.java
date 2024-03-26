a080e48690baf4f8897b863cb28238951a406b4c
박예원 <parkyw0321@gmail.com>
2024-03-26 10:59:15 +0900

    fix(diff): sonarqube 기반 1차 리팩토링
    Former-commit-id: c4e0938c25337f022e15cd9113b810a05edae02b

--- back/knpl/src/main/java/com/b301/knpl/repository/DIFFRepository.java
+++ back/knpl/src/main/java/com/b301/knpl/repository/DIFFRepository.java
@@ -1,9 +1,6 @@
 package com.b301.knpl.repository;
 
 import com.b301.knpl.entity.Custom;
-import com.b301.knpl.entity.File;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
