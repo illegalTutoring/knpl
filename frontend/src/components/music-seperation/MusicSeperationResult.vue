671b597fad26c4e855e430fbc5fb848fbb0f0863
정종욱 <cnhug3@naver.com>
2024-03-27 13:53:25 +0900

    fix(api): 분리된 음성 전달하기 get요청 수정.
    Former-commit-id: 764704e52fd1602c8779eaf56f915fd5cfefa811

--- frontend/src/components/music-seperation/MusicSeperationResult.vue
+++ frontend/src/components/music-seperation/MusicSeperationResult.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9bccd1cbdbe869f387cac86e673df1b19b4531bab7fe610b323e909e3edf3527
+oid sha256:54a75366fb13d22dfd0bf3ea6c5ffcf9be22b2949992e6afb7bd8486b5622a10
 size 11170
