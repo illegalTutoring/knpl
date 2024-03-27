d73e911819e11803c0e758e01f6fdab9d71a4db8
정종욱 <cnhug3@naver.com>
2024-03-27 12:51:49 +0900

    fix(savemodal): bassFile 다운로드 url 수정.
    Former-commit-id: 6dd2a612172d52313521ce5c7b12acd9963a061f

--- frontend/src/components/music-seperation/MusicSaveModal.vue
+++ frontend/src/components/music-seperation/MusicSaveModal.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:36d5460653dd781866e4953a8e7f1a02161f3d8185020aed6a92976208d858b7
+oid sha256:f05e0d6009b10febc6cd58d1a5d80a5ec27b6ce8ef223cd488e189a0d240cd1f
 size 2545
