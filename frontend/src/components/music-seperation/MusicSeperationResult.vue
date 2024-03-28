e15500fef266e5be1a53db0a9dda864d2eacf9eb
오철수 <target511@kakao.com>
2024-03-28 15:03:29 +0900

    feat(separation_result): MusicSeparation 결과 페이지 디자인 수정
    Former-commit-id: c2bb56307d7a286e119c11040a76013d29f7073b

--- frontend/src/components/music-seperation/MusicSeperationResult.vue
+++ frontend/src/components/music-seperation/MusicSeperationResult.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:40ec96a4586901beaa958288fdbaa3138e0cbe819cf92884d1b9e5d8e2ea297c
-size 11252
+oid sha256:5b86b45152098d706aa2adc23e7870314a7cab6cfaa8dc849773aa762c234469
+size 10941
