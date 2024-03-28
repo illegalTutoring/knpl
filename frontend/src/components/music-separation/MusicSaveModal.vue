812daa56225b09728db90aacb936f7afc8de041a
정종욱 <cnhug3@naver.com>
2024-03-28 16:47:54 +0900

    fix(muscisave): 음악 다운로드시 설정한 이름으로 다운로드.
    Former-commit-id: 953410718efda314fed849ccc0c98bca4a5fbd38

--- frontend/src/components/music-separation/MusicSaveModal.vue
+++ frontend/src/components/music-separation/MusicSaveModal.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1240f653cbb69a15298b4b05e920b50c4bdd0e002684e0ca950197ab0827e8b2
-size 2360
+oid sha256:86a167309830dd5ace2ab2cd041ece205c9cceea56ac60d94362dbc2603bee8d
+size 2950
