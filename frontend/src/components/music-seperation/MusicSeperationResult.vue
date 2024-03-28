1b30e6df72c0cb592df1d865eb1fb401f3f40a59
정종욱 <cnhug3@naver.com>
2024-03-28 14:21:48 +0900

    fix(seperationresult): 저장 버튼 클릭시 모달창 안띄워지는 오류 해결.
    저장버튼을 눌렀더라도 모달창이 띄워지지 않은 오류 발생.
    해당 오류가 난 로직 수정 완료.
    
    
    Former-commit-id: 9bfd4936eba6bb680bbef71ef2a527bbf85ad8bc

--- frontend/src/components/music-seperation/MusicSeperationResult.vue
+++ frontend/src/components/music-seperation/MusicSeperationResult.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd8f5d5098b67b880baa7ad522302a53155a63bfc2597b711c170314f69f9e0b
-size 11511
+oid sha256:39fa542d2f6a17bdeed99b4be32734864c6dbf7449f01156c2b416689b22ebcf
+size 10896
