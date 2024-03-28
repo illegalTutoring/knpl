9e7cae7282ab4bbb29ce56ffd15bc267c3f5e72b
조영진 <tl5235566@gmail.com>
2024-03-28 21:49:55 +0900

    feat(ux_fix_08): 홈 화면 디자인 수정
    - 캐러셀 조정
    - 글자 폰트 조정
    - 캐러셀 인디케이터 변경
    
    
    Former-commit-id: a1c5ec2ca06a705e9a5cf213acc42e492c91f151

--- frontend/src/components/image-slide/ImageSlide.vue
+++ frontend/src/components/image-slide/ImageSlide.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f99c4e93f5e44a8f32a4cb133e1041fe7172a6fd0670b6227c03cb0c91bb35cb
-size 3670
+oid sha256:aac8bdcf83ef3efc03dabe6e7be0afb1f396547a1110240536b78a4bc3a1963e
+size 3304
