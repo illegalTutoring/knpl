c2060c7e23509f5d0a3b223945f7de543ff28f9e
정종욱 <cnhug3@naver.com>
2024-03-29 14:35:59 +0900

    feat(conversion): AI Cover 설명창 추가.
    Former-commit-id: 01abc72c73ca937dd454d79808f9a4bf967079e3

---
+++ frontend/src/components/music-conversion/ConversionManual.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="conversion-manual">
+        <p>사용자의 mp3, wav 파일과 Kim,na,park,lee 중 하나를 선택합니다.</p>
+        <p>해당 파일을 선택한 가수가 부른것 처럼 목소리를 바꿔주고 이를 저장할수 있습니다.</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+import "@/components/music-conversion/music-conversion-title.css";
+</script>
--- frontend/src/components/music-conversion/MusicConversionMain.vue
+++ frontend/src/components/music-conversion/MusicConversionMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c53475c658137ac7efa96fa3ea9ff0ddcf8b9ee3908f7ad0b7b7905bcbcd07c
-size 4142
+oid sha256:cb8e96573945a97e34caf79b4d69d6415818e5fb5267bffd0d9b06dda7513431
+size 4409
