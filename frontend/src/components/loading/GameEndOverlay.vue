55371c0c1dfd02290ec911fb8200714c37b6c9ab
김태희 <apples5060@naver.com>
2024-03-29 17:55:40 +0900

    fix(game): 2048 css 수정
    1. GameEndOverlay css class 중복 수정
    2. SonarQube 이슈 수정
    
    
    Former-commit-id: b7205a3d46cea7ffda4f532812418c21a103561f

--- frontend/src/components/loading/GameEndOverlay.vue
+++ frontend/src/components/loading/GameEndOverlay.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9aac9883d2b72c721fa624075f037183a36e01220f389fd829b4d26d93b74472
-size 1138
+oid sha256:67ca128e221ef093a322da9ca37b8a0f317deb92889bb225d4f6c6f5afc83c84
+size 1155
