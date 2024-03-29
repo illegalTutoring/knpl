fd44d0980b1250e6bfefcc9793ee7f317f775cbb
오철수 <target511@kakao.com>
2024-03-29 16:23:14 +0900

    fix(separation_download): 세션 음성파일만 다운되는 버그 수정
    Former-commit-id: fea84aef68a53f8dc82ea02e3a6d71aaf7d7378a

--- frontend/src/components/loading/Cell.vue
+++ frontend/src/components/loading/Cell.vue
@@ -1,3 +1,5 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ad1f2c026fd03e12e9efd611e9f4ccd4dc38dc48b845a29b9bb8f98df741a4a9
-size 85
+<template>
+  <span class="cell">{{ "" }}</span>
+</template>
+
+<script setup></script>
