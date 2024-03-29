75412b0fb90616b0aa7a7fa806b17ac7cc0bdcca
정종욱 <cnhug3@naver.com>
2024-03-29 13:40:18 +0900

    feat(separationmanual): separation 설명창 추가.
    i 이모지 마우스 가져다 오면 설명 modal창 띄우기.
    
    
    Former-commit-id: c42270739468b533f977c82e0e6fbcad1a7916a1

--- frontend/src/components/loading/Cell.vue
+++ frontend/src/components/loading/Cell.vue
@@ -1,3 +1,12 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c3d6977a69df7649063d39288b190cff118aded92531ad63bf75fb60e653cb14
-size 149
+<template>
+  <span class="cell">{{ "" }}</span>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {}
+  },
+  components: {},
+};
+</script>
--- frontend/src/components/music-separation/MusicSeparationMain.vue
+++ frontend/src/components/music-separation/MusicSeparationMain.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef73ecfa31f888f5538531432d51face821f87b365b5b786c83345fa5f0330a2
-size 3385
+oid sha256:08fed035f815342b42f6c7153ace4a1b5613ff0f356b0f21028256b8767137f0
+size 4124
