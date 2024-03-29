61b65740bfc2589229f380c3735d6bc317588a4e
김태희 <apples5060@naver.com>
2024-03-29 11:24:26 +0900

    feat(loading): 로딩 페이지 2048 게임 추가
    1. 로딩 페이지 2048 게임 추가
    2. tsconfig.app.json 에서 자바스크립트 빌드 오류 처리 완료
    3. 패키지 autoprefixer, sass 설치
    
    
    Former-commit-id: d3387f073e8afb9e571980d997e5d65402e7365e

--- diff/modules/parallel_wavegan/models/__init__.py
+++ diff/modules/parallel_wavegan/models/__init__.py
@@ -1,2 +1,3 @@
-from .melgan import *  # NOQA
-from .parallel_wavegan import *  # NOQA
+version https://git-lfs.github.com/spec/v1
+oid sha256:e89ecf0f3c18bbaf31b83fac321a4ac4c5eddabb14af8a9296a688fc06f1ae62
+size 70
--- frontend/src/views/CheckResultView.vue
+++ frontend/src/views/CheckResultView.vue
@@ -1,13 +1,3 @@
-<template>
-    <div>
-        <div>
-            <CheckResult/>
-        </div>
-    </div>
-    
-</template>
-
-<script setup lang="ts">
-import CheckResult from '@/components/check-result/CheckResult.vue';
-
-</script>
+version https://git-lfs.github.com/spec/v1
+oid sha256:0a0adcf69554b36c8046c96114081d0cbeb84721848338d7c222769d1cf09cf6
+size 211
