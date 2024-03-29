712c34ec6ce8a0786c49b02c0721febb021d5012
김태희 <apples5060@naver.com>
2024-03-29 15:45:04 +0900

    feat(game): 로딩페이지 2048게임 추가 및 패키지 설치
    1. sass, autoprefixer 설치
    2. scss 부모 설정
    3. 로딩페이지 2048 게임 추가
    
    
    Former-commit-id: 46f7fc89ae5fe6e664f67c0d4090aad33ea976b6

--- frontend/src/components/loading/GameEndOverlay.vue
+++ frontend/src/components/loading/GameEndOverlay.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:86037c8d8d6607bfb17b96f8745b364a136afc1bc19a1a03402b592fc49e3f32
-size 928
+oid sha256:207123fa4478530e65f108cb075abaf42150542466ec85378ebf0ca528efb23f
+size 1036
