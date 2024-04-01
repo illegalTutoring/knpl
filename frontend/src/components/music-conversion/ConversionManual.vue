03fcdabbd576e21825c4f6d64312a39ffdd1cb25
정종욱 <cnhug3@naver.com>
2024-04-01 09:25:15 +0900

    fix(manual):  div 구조 변화 및 CSS 조정
    메뉴얼 CSS 조정.
    
    
    Former-commit-id: ea1098c81dc7b54bd22caad5d14fbd444649a660

--- frontend/src/components/music-conversion/ConversionManual.vue
+++ frontend/src/components/music-conversion/ConversionManual.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec179c22dda2f0c587c69ea695788fd24b0957a577dd9ebe250f27747a00e60d
-size 1432
+oid sha256:ec0a3a410e9114fdca402631d1bbdcc008bdef7c5cccca29331e635ed406e406
+size 1351
