9911da82a0c3eecc5a7b757b2364f17a8a0599bd
박예원 <parkyw0321@gmail.com>
2024-04-01 14:34:38 +0900

    feat(loading-design): eventsource mime 문제 해결
    Former-commit-id: f3da43919f86e3b6b20d5fc9cf9d392fb8e756cb

--- frontend/src/stores/conversion.ts
+++ frontend/src/stores/conversion.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a4961bf56fbd73858d9c93e594cdf2d2c222246e296226c81be1a23b1a0dd96
-size 3661
+oid sha256:0ca8bc17f028be3bdaccc05833083461147309258d32f185a7c585ae0f23a411
+size 3652
--- frontend/src/stores/separate.ts
+++ frontend/src/stores/separate.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0af74880dc051b1e7d99eedcf7596b8ad4b6026a1c927aea4c6e18d349104dbc
-size 15132
+oid sha256:73fbca3ca0b33f3e332ce24e0cdf26ff29d1abd3872cea8b61a1181e88f2a915
+size 15123
