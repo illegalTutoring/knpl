51c9ce497654e46b719211cf377a68ed1ce84d46
오철수 <target511@kakao.com>
2024-04-01 15:42:33 +0900

    feat(ai-cover): equalizer 디자인 추가
    Former-commit-id: 08d7b79d87f1914a640fae75b2378c9b9f180423

--- frontend/src/components/music-conversion/MusicConversionResult.vue
+++ frontend/src/components/music-conversion/MusicConversionResult.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e883e68588b8915c8e241be6f9c87146c4681a481bb4237439ef4b91d5aaec28
-size 7218
+oid sha256:7e1ccb07ce9c58803e88ee5de8a237702094269499513b93008bf2ba41ff24b0
+size 6746
