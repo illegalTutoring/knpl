fb127a18dc1d3f2e9506ac2f8d08c15fab2b44b6
박예원 <parkyw0321@gmail.com>
2024-04-01 16:24:50 +0900

    feat(loading-design): css 수정, index.html head 로고 타이틀 수정
    Former-commit-id: be25352b35ac4c5d0dca81dbe81b10791966db68

--- frontend/src/components/loading/Loading.vue
+++ frontend/src/components/loading/Loading.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f6b8326bf68becf8fc9ed787cb1d584dbb44b2e4f31203e194d2750fd245ef1
-size 1671
+oid sha256:e9255e6b1e396d919b5297431faeb1bcc157904064ee03aee4f9ed79536fed09
+size 1668
--- frontend/src/stores/loading-alarm.ts
+++ frontend/src/stores/loading-alarm.ts
@@ -1,3 +1,8 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9f08d589ec7952a8ca1d1abf3103a7dc7135a722977d4d01b35805b8a8434a10
-size 202
+import { ref } from 'vue'
+
+import { defineStore } from 'pinia'
+
+export const useLoadingAlarmStore = defineStore('loadingalram', () => {
+    const loadingAlarm = ref(false)
+    return { loadingAlarm }
+})
