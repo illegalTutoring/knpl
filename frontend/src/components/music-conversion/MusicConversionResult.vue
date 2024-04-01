3bf57740acfe7bb967abf2131c42d8c515802868
오철수 <target511@kakao.com>
2024-04-01 15:49:25 +0900

    feat(ai-cover): volume container 디자인 추가
    Former-commit-id: 25b022e8466593b03a163e8ff5b1a8b9e3291dd1

--- frontend/src/components/music-conversion/MusicConversionResult.vue
+++ frontend/src/components/music-conversion/MusicConversionResult.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e1ccb07ce9c58803e88ee5de8a237702094269499513b93008bf2ba41ff24b0
-size 6746
+oid sha256:d1c2811bc7f0b9dd8252ddd52f98eb0d4a437a0845981e533ea8543d17d836a0
+size 6871
