92c2fbe1fe8a2123c993cb05b8c7034a75f900fa
정종욱 <cnhug3@naver.com>
2024-04-02 12:58:21 +0900

    fix(Loading): 사용하지 않는 변수, 스타일 코드 제거.
    Former-commit-id: ac7687c9f4ece801663dedca465a09092f5ec728

--- frontend/src/components/loading/Loading.vue
+++ frontend/src/components/loading/Loading.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e9255e6b1e396d919b5297431faeb1bcc157904064ee03aee4f9ed79536fed09
-size 1668
+oid sha256:d4ec1a710d16d42e95addaa5aa73d77a7be7da22270fbeb47be7726e146b762a
+size 1440
