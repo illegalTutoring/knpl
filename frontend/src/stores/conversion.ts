b522b41cfeeea660fbe16f7e6b5c584e8bfb83dd
정종욱 <cnhug3@naver.com>
2024-04-02 14:17:58 +0900

    feat(cookie): 메뉴얼 관련 쿠키 설정.
    쿠키 지속시간 1시간.
    
    
    Former-commit-id: 24d3f580ab16bbb656236bae3cbb80088cb2c286

--- frontend/src/stores/conversion.ts
+++ frontend/src/stores/conversion.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ad273e85ab7b5937b0d1d763554dd175b93a4800322e3aa5e49af2d73461d2e
-size 3709
+oid sha256:0ca8bc17f028be3bdaccc05833083461147309258d32f185a7c585ae0f23a411
+size 3652
